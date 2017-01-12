e2e8e16ddc145a1e3d1df5c1e1030d769bc6b95e
Fabiola Ampuero <fabi.ampuero.chaclco@gmail.com>
2017-01-12 17:12:41 -0500

    pruebas

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function esp(){
+    var formulario=document.createElement("form");
+    var titulo=document.createElement("h2");
+    var contenido=document.createTextNode("Por favor inicia sesión");
+    titulo.appendChild(contenido);
+    formulario.setAttribute("margin","0 auto");
+    titulo.setAttribute("text-align","center");
+    document.getElementById("form-signin-heading").appendChild(titulo);
+}

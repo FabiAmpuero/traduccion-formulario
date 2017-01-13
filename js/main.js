7c6646c35b362fcce58ab7b5825ac23129060f62
Fabiola Ampuero <fabi.ampuero.chaclco@gmail.com>
2017-01-13 13:12:31 -0500

    ejercicio terminado

--- js/main.js
+++ js/main.js
@@ -19,13 +19,37 @@ function esp(){
     document.getElementById("form-signin-heading").appendChild(titulo);
 }
 */
+/*creacion de boton*/
+var boton=document.createElement("button");
+var contenido=document.createTextNode("Español");
+boton.appendChild(contenido);
+boton.setAttribute("onclick","esp()");
+document.getElementsByClassName("container")[0].appendChild(boton);
+
+var botonn=document.createElement("button");
+var contenido=document.createTextNode("Ingles");
+botonn.appendChild(contenido);
+botonn.setAttribute("onclick","ing()");
+document.getElementsByClassName("container")[0].appendChild(botonn);
+
+/*creacion de boton*/
 function esp(){
     var titulo=document.getElementById("form-signin-heading");
     titulo.innerHTML="Por favor inicia sesión";
     var email=document.getElementsByClassName("form-control")[0].placeholder="Correo Electrónico";
-    var password=document.getElementsByClassName("form-control")[1].placeholder="Contraseña";
+    var contrasena=document.getElementsByClassName("form-control")[1].placeholder="Contraseña";
     
     var span=document.getElementsByTagName("span")[0].innerHTML="Recordar datos";
     
-    var boton=document.getElementsByClassName("btn").innerHTML="porfi";
+    var boton=document.getElementsByClassName("btn")[0].innerHTML="Iniciar Sesión";
+}
+function ing(){
+    var title=document.getElementById("form-signin-heading");
+    title.innerHTML="Please sign in";
+    var email=document.getElementsByClassName("form-control")[0].placeholder="Please enter your email";
+    var password=document.getElementsByClassName("form-control")[1].placeholder="Password";
+    
+    var spanDos=document.getElementsByTagName("span")[0].innerHTML="Remember me";
+    
+    var buton=document.getElementsByClassName("btn")[0].innerHTML="Sign in";
 }
